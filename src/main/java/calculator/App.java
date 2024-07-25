046e7b3ba02a27acc4cb896e44586ab2ce4db24a
leeminhyeok <lklll1212@gmail.com>
2024-07-25 14:04:06 +0900

    Level1 -4
    do -while 을 이용한 반복문 생성.
    마지막에 exit 를 입력 시 종료.
    String textString; // 선언
    textString = sc.next(); // 문자라서 int x
    while(! textString.equals("exit");
    // 입력하기 전까지 반복! 끝

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,74 +1,99 @@
 package calculator;
+
 import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int num1 = sc.nextInt();
-
-        System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
-        /* charAt(idx) : charAt 메서드는 메개변수로 char 타입으로 변환 하고자하는 문자열의 위치(index)를 받는다*/
-        char operator = sc.next().charAt(0);
-        //switch 문을 통해 기호에 맞는 연산이 되도록 설정.
-
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
-        // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        // textString 이라는 문자를 사용하기 위한 선언. "exit" 라는 문자를 받기 위함입니다.
+        String textString;
 
 
-        // 선언. 강의 2-7 int month= 8을 참고했습니다.
-        int result =0;
+        // 반복문 do -while 을 사용했습니다.while 이 false 가 나오기 전까지 반복됩니다.
+        // 반복 여부 상관없이 첫번째 계산값은 나타내고 싶어서 do -while 을 사용했습니다.
 
-        //사칙연산 기호를 저장한 변수 = operator
-        switch (operator){
 
-        // case 뒤에는 조건!! 변수에 따라 뒤에 result 값이 이렇게 계산되고, break 되도록 설정했습니다.
-        case '+':
-            result = num1 + num2;
 
-            break;
+        do {
 
-        case '-':
-            result = num1 - num2;
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int num1 = sc.nextInt();
 
-            break;
+            System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
+            /* charAt(idx) : charAt 메서드는 메개변수로 char 타입으로 변환 하고자하는 문자열의 위치(index)를 받는다*/
+            char operator = sc.next().charAt(0);
+            //switch 문을 통해 기호에 맞는 연산이 되도록 설정.
 
-        case '*':
-            result = num1 * num2;
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+            // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
 
-            break;
 
-        case '/':
+            // 선언. 강의 2-7 int month= 8을 참고했습니다.
+            int result = 0;
 
-            if (num2 != 0){
-                result = num1 / num2;
+            //사칙연산 기호를 저장한 변수 = operator
+            switch (operator) {
 
-            }else {
-                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                //결과값을 아예 텍스트로 표현하고 싶지만 result 는 int result 로 선언했기 때문에 숫자로 입력했습니다.
-                result = 999999999;
-                break;
+                // case 뒤에는 조건!! 변수에 따라 뒤에 result 값이 이렇게 계산되고, break 되도록 설정했습니다.
+                case '+':
+                    result = num1 + num2;
+
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+
+                    break;
+
+                case '/':
+
+                    if (num2 != 0) {
+                        result = num1 / num2;
+
+                    } else {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        //결과값을 아예 텍스트로 표현하고 싶지만 result 는 int result 로 선언했기 때문에 숫자로 입력했습니다.
+                        result = 999999999;
+                        break;
+
+
+                    }
+                    // 위에 값 중 예상된 값이 입력되지 않을 경우. ex. 기호 자리에 숫자입력.
+                default:
+                    System.out.println("잘못 입력하셨습니다.");
+
 
 
             }
+            System.out.println("결과: " + result);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+
+            // 위에 있는 int num2 = sc.nextInt(); 참고했고,
+            // 숫자가 아니라서 Int 는 지우고 적어봤습니다.
+            textString = sc.next();
 
 
 
-        //default
-
-            // result 를 입력하려다 자동완성 return 을 발견했습니다.
-            // 이거 쓰면 전에 했던 동작으로 돌아갈 줄 알았는데 그게 아니라서 깔끔한 마무리가 되는거같아요!
-}
 
 
-        System.out.println("결과: " + result);
 
-        }
+            // 2주차 강의 2-6 if 에서 가위바위보 Objects.eqauls 를 참고해서 넣었습니다.
+            // 해당 강의에서는 nextLine 을 사용했었는데
+            //  sc.nextLine 으로 입력하니 더 계산하시겠습니까? 에 답을 입력 못해서 수정했습니다.
+          }while(!  textString.equals("exit"));
 
-
+        };
 
     }
+
+
+
+
 
