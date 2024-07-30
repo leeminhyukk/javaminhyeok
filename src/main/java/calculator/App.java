dcd0b64d7dbd02ee70aa1f4ca5561364b54d88b4
leeminhyeok <lklll1212@gmail.com>
2024-07-30 18:13:51 +0900

    level2-2 Calculator 클래스에서 연산을 수행하도록 설정. Calculator plus = new Calculator(); 로 생성했기 때문에. plus. 메서드 이런식으로 써야함. 연산값도 Calculator 필드에 저장됨.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,34 +7,25 @@ import java.util.Scanner;
 
 public class App {
 
+
     public static void main(String[] args) {
+        double result =0;
         Scanner sc = new Scanner(System.in);
         // textString 이라는 문자를 사용하기 위한 선언. "exit" 라는 문자를 받기 위함입니다.
-        String textString;
+        String textString ="";
         String removeString;
         // removeString 이라는 문자를 사용하기 위한 선언. "remove" 라는 문자를 받기 위함입니다. 1-7
-        // 위쪽에 선언을 지우고 밑에서 값을 입히면서
-        // String removeString = sc.next(); 로 하려 했으나 textString 이 오류가 남.
+
         String inquiryString;
-        Calculator Calc = new Calculator();
+        // 메서드 외 접근 호출.
+        Calculator plus = new Calculator();
 
-
-
-
-
-        // 10개짜리 배열 선언,생성 / 저장하는 변수 선언. count
-        // int intArray[] = new int[10];
-        // int count = 0; 정적 배열은 1-7 을 수행하기 위해 주석처리 했습니다.
 
         //1-7 무한이 저장될 수 있도록 동적 배열 생성했습니다.
         ArrayList<Integer> intList =new ArrayList<Integer>();
 
-
-
         // 반복문 do -while 을 사용했습니다.while 이 false 가 나오기 전까지 반복됩니다.
         // 반복 여부 상관없이 첫번째 계산값은 나타내고 싶어서 do -while 을 사용했습니다.
-
-
 
         do {
 
@@ -52,63 +43,54 @@ public class App {
             // Scanner 를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
 
             // 선언. 강의 2-7 int month= 8을 참고했습니다.
-            int result = 0;
+
 
             //사칙연산 기호를 저장한 변수 = operator
 
-                    // 위에 값 중 예상된 값이 입력되지 않을 경우. ex. 기호 자리에 숫자입력.
 
+        switch (operator) {
+            // case 뒤에는 조건!! 변수에 따라 뒤에 result 값이 이렇게 계산되고, break 되도록 설정했습니다.
+            case '+':
+                plus.addCalculate(num1,'+',num2);
+                break;
+            case '-':
+                plus.subCalculate(num1,'-',num2);
+                break;
+            case '*':
+                plus.mulCalculate(num1,'*',num2);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    plus.divCalculate(num1,'/',num2);
+                } else {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    //결과값을 아예 텍스트로 표현하고 싶지만 result 는 int result 로 선언했기 때문에 숫자로 입력했습니다.
+                    throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
 
+                }
 
+                // 위에 값 중 예상된 값이 입력되지 않을 경우. ex. 기호 자리에 숫자입력.
+            default:
+            {System.out.println("잘못 입력하셨습니다.");
+                break;} }
 
-            System.out.println("결과: " + result);
+            System.out.println("결과: " + plus.result);
 
-            // 새로 생성된 동적 배열에 결과값 넣기.
-            intList.add(result);
-
-            //intArray[count]=result; 정적 배열 결과값 넣던 방식 주석처리.
-
-            //count++; 어차피 값이 쭉쭉 들어간다.
-
-
-            // if 10개가 꽉 찼을 때, for 문을 사용해서 앞으로 한 칸 씩 밀어줬습니다.
-            // count 값을 9로 설정해서. 마지막 칸에 새로운 결과값이 입력되도록 했습니다.
-//            if(count == intArray.length) {
-//
-//                for (int i = 0; i < intArray.length-1; i++) {
-//                    intArray[i] = intArray[i + 1];
-//                    System.out.println(" 저장 공간이 꽉 차서 앞으로 한칸씩 이동합니다.");
-//                   count = 9;
-//                }
-//                // intArray[] 안에 9로 넣어도 결과는 같습니다!
-//                intArray[intArray.length-1] = result;
-//
-//
-//            }
             removeString = sc.nextLine(); // 안에 있던 enter 값 삭제를 위해. (버퍼)
             // remove 라는 값을 입력받으면 가장 먼저 입력된 결과가 삭제될 수 있도록 구현. 첫번째 인덱스 intList.remove(0);
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
              removeString = sc.nextLine();
             if( removeString.equals("remove")) {
-                intList.remove(0);
+                plus.doubleList.remove(0);
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             inquiryString = sc.nextLine();
             if(inquiryString.equals("inquiry")){
-                for(int list: intList){
+                for(double list: plus.doubleList){
                     System.out.println(list);
                 }
             }
-
-
-
-
-
-
-
-
-
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
 
@@ -119,14 +101,15 @@ public class App {
             // 2주차 강의 2-6 if 에서 가위바위보 Objects.eqauls 를 참고해서 넣었습니다.
             // 해당 강의에서는 nextLine 을 사용했었는데
             //  sc.nextLine 으로 입력하니 더 계산하시겠습니까? 에 답을 입력 못해서 수정했습니다.
-          }while(!  textString.equals("exit"));
 
 
 
 
-        };
+
+        }while(!(textString.equals("exit")));
 
     }
+}
 
 
 
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,69 +6,87 @@ public class Calculator {
 
 
     //연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
-    ArrayList<Integer> intList =new ArrayList<Integer>();
+    ArrayList<Double> doubleList =new ArrayList<>();
     //받을 숫자 2개 생성 num1 첫번째, num2 두번째
     int num1 =0;
     int num2 =0;
     double result =0;
     char operator;
 
+
     //반환 타입,결과값 double 로 변경.
-    public double calculate(int num1, char operator, int num2) {
-        switch ()
-        result = num1 +num2;
+    // 결과값 변수 초기화.
 
-}
+    // 연산 기호를 매개변수로 받아 사칙연산(+,-,*,/) 기능을 수행한 후 결과 값을 반환하는 메서드 4개 add , sub, mul , div
 
-        // 결과값 변수 초기화.
-        double result = 0;
+    public double addCalculate(int num1,char operator, int num2){
+        result = num1 + num2;
+        return result;
+    }
 
-        double operator(char type) {
+    public double subCalculate(int num1,char operator, int num2){
+        result = num1 - num2;
+        return result;
+    }
 
+    public double mulCalculate(int num1,char operator, int num2){
+        result = num1 * num2;
+        return result;
+    }
 
-        // 연산 기호를 매개변수로 받아 사칙연산(+,-,*,/) 기능을 수행한 후 결과 값을 반환하는 메서드
-
-        double add(num1, num2){
-            result = num1 + num2;
-            return result;
+    public double divCalculate(int num1,char operator, int num2){
+        if (num2 != 0){
+        result = num1 / num2;
+        return result;
+        } else {
+        throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
         }
 
-        double sub(num1, num2){
-            result = num1 - num2;
-            return result;
-        }
 
-        double mul(num1, num2){
-            result = num1 * num2;
-            return result;
-        }
 
-        double div(num1, num2){
-            if (num2 != 0)
-            result = num1 / num2;
-            return result;
-            if (num2 = 0)
-            throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-        }
+    }
+    public void addList(){
+        doubleList.add(result);
+    }
 
                     //예외처리. 구글링 참고
 
 
 
-                }
-                intList.add(result);
-
-            default:
-                System.out.println("잘못 입력하셨습니다.");
-
-
-        }
 
 
 
 
 
-
-
-    }
 }
+
+
+
+//        switch (operator) {
+//            // case 뒤에는 조건!! 변수에 따라 뒤에 result 값이 이렇게 계산되고, break 되도록 설정했습니다.
+//            case '+':
+//                result = num1 + num2;
+//                break;
+//            case '-':
+//                result = num1 - num2;
+//                break;
+//            case '*':
+//                result = num1 * num2;
+//                break;
+//            case '/':
+//                if (num2 != 0) {
+//                    result = num1 / num2;
+//                } else {
+//                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+//                    //결과값을 아예 텍스트로 표현하고 싶지만 result 는 int result 로 선언했기 때문에 숫자로 입력했습니다.
+//                    result = 999999999;
+//                    break;
+//                }
+//                intList.add(result);
+//                // 위에 값 중 예상된 값이 입력되지 않을 경우. ex. 기호 자리에 숫자입력.
+//            default:
+//                System.out.println("잘못 입력하셨습니다.");
+//        switch ()
+//        result = num1 +num2;
+
+
